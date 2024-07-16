827ea51ec4a1c2d09ce1e317edfa3d4f1776a14f
Mumuksha Pant <mumukshapan97@gmail.com>
2024-07-15 20:25:38 -0400

    Time: 5 ms (76.91%), Space: 45.6 MB (12.16%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
@@ -1,22 +1,29 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
         //since the constraints are very small , only 101 , we can use brute force
-int[] n1= new int[101]; 
-int[] n2= new int[101]; 
+int count=0; 
 int[] res= new int[2];
-        for(int i: nums1)
-            n1[i]++; 
-
-        for(int i:nums2)
-            n2[i]++; 
-
-        for(int i=0;i<101;i++){
-            if(n1[i]!=0 && n2[i]!=0)
-                {
-                    res[0]+=n1[i]; 
-                    res[1]+=n2[i]; 
+        for(int i=0;i< nums1.length; i++){
+              for (int j = 0; j < nums2.length; j++) {
+                if(nums1[i]==nums2[j]){
+                    count++; 
+                    break;
                 }
+              }
         }
+        res[0]=count; 
+
+        //reset count=0 for nums2 common 
+count=0; 
+        for(int i=0;i< nums2.length; i++){
+              for (int j = 0; j < nums1.length; j++) {
+                if(nums2[i]==nums1[j]){
+                    count++; 
+                    break;
+                }
+              }
+        }
+        res[1]=count; 
 
         return res; 
     }
