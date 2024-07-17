f2fbb75e3f5729366ab46ab8aaf6e4bc3dfff2a4
Mumuksha Pant <mumukshapan97@gmail.com>
2024-07-17 14:24:55 -0400

    Time: 5 ms (26.39%), Space: 45 MB (75.52%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,21 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n= nums.length; 
-        int sumofnnumbers= (n*(n+1))/2; 
+        Arrays.sort(nums);
+        int n = nums.length;
+        int low = 0, high = n-1;
+         
 
-        int sum=0;
+        while (low <= high) {
+           int mid = low+( high-low) / 2;
+            if (nums[mid] <= mid) 
+                low = mid+1;
 
-        for(int i:nums)
-            sum+=i; 
+             else 
+                high = mid -1;
+            
+           
+        }
 
-        return sumofnnumbers-sum;
+        return low;
     }
 }
