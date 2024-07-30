299e8ac244e25fdbde333c6f96e8a23b3c930b53
Mumuksha Pant <mumukshapan97@gmail.com>
2024-07-30 12:31:29 -0400

    Time: 66 ms (23.71%), Space: 45.5 MB (22.61%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.java
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.java
@@ -1,30 +1,36 @@
 class SmallestInfiniteSet {
+    //ref: https://www.youtube.com/watch?v=7hAmykNYdPc
 PriorityQueue<Integer> pq; 
-Set<Integer> set ; 
+// let us remove set , we dont need it because pq in it self provides u
+
+//The code works without the Set<Integer> set because the PriorityQueue<Integer> pq itself can ensure the functionality required for the SmallestInfiniteSet class.
+
+
+// Set<Integer> set ; 
 
     public SmallestInfiniteSet() {
         pq= new PriorityQueue<>(1000); 
-        set= new HashSet<>(1000); 
+        //set= new HashSet<>(1000); 
 
         for(int i=1;i<=1000; i++){
             pq.add(i); 
-            set.add(i); 
+           // set.add(i); 
         }
     }
     
     public int popSmallest() {
         
         int x= pq.poll(); 
-        set.remove(x); 
+        //set.remove(x); 
         return x; 
         
     }
     
     public void addBack(int num) {
 
-        if(!set.contains(num)){
+        if(!pq.contains(num)){
             pq.add(num); 
-            set.add(num); 
+            //set.add(num); 
         }
         
     }
