6d31e040b20c20adab7a4ae12906f5a8da3b7060
Mumuksha Pant <mumukshapan97@gmail.com>
2024-08-06 10:41:08 -0400

    Time: 2 ms (78.86%), Space: 45.4 MB (66.35%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,20 +1,20 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-
-//start from last
-        int goal = nums.length-1; 
-
-        for(int i=nums.length-1;i>=0; i--){
-      
-            int nextpos= nums[i]+i; 
-
-            if(nextpos>=goal)
-                goal=i; 
+    public boolean canJump(int[] nums) {     //start from i=0;
 
 
+        int maxReach = 0; 
+
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (i > maxReach) //curr index is beyond the max we can reach
+                return false;
+
+            maxReach = Math.max(maxReach, i + nums[i] );
+
+            if (maxReach >= nums.length - 1) 
+                return true;
+            
         }
-        
-
-        return goal==0; 
+        return false;
     }
 }
