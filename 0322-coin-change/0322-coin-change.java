20f4de7089fe0571b6a32fb9d982ec2dd4dd3d50
Mumuksha Pant <mumukshapan97@gmail.com>
2024-08-30 10:03:10 -0400

    Time: 16 ms (60.67%), Space: 44 MB (83.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,21 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
 
-        //ref : https://www.youtube.com/watch?v=NNcN5X1wsaw
-
-        // dp represents the number of coins to make an amount i
         if(amount<1)
-            return 0;
+        return 0; 
 
-        int[] dp = new int[amount + 1];
-        dp[0] = 0;
+        //amount 
 
-        for (int i = 1; i < dp.length; i++) {
-            dp[i] = Integer.MAX_VALUE;
+        int[] dp = new int[amount+1]; 
+        dp[0]=0; 
 
-            for (int c : coins) {
-                if (c <= i && dp[i - c] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
+
+        for(int i=1;i<dp.length; i++){
+            dp[i]=Integer.MAX_VALUE; 
+
+            for(int c: coins){
+                if(c<=i && dp[i-c]!=Integer.MAX_VALUE){
+                    dp[i]= Math.min(dp[i], dp[i-c]+1); 
                 }
             }
         }
@@ -24,6 +24,6 @@ class Solution {
             return -1; 
 
         return dp[amount]; 
-
+        
     }
 }
