e9297bafa84beee2e03ccfe9393ab1d43b021e65
Mumuksha Pant <mumukshapan97@gmail.com>
2024-08-30 10:23:01 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,31 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st= new Stack<>(); 
+
+        Stack<Character> st = new Stack<>(); 
         if(s.length()==1)
-            return false; 
-        
+        return false; 
 
-        for(int i=0;i<s.length();i++){
+        for(char i=0; i<s.length(); i++){
+            char ch = s.charAt(i) ; // (
 
-           
-                if(!st.isEmpty() && s.charAt(i)==')' && st.peek()=='(' )
-                    st.pop(); 
+                if(ch=='(' || ch=='[' || ch=='{')
+                    st.push(ch); 
 
-                 else if(!st.isEmpty() && s.charAt(i)==']' && st.peek()=='[' )
-                    st.pop(); 
+                if(ch==')' || ch==']' || ch=='}')
+                {
+                    char top= st.pop(); 
 
-                 else if( !st.isEmpty() && s.charAt(i)=='}' && st.peek()=='{' )
-                    st.pop(); 
-
-                else 
-                st.push(s.charAt(i));
+                    if (   (top=='(' && ch!=')') || 
+                    (top=='[' && ch!=']')  || (top=='{' && ch!='}') )
+                    return false; 
 
 
-        
 
-        
-}
-        return st.isEmpty();
+                }
+            
+
+        }
+
+        return st.isEmpty(); 
         
     }
 }
