f2326b366db367841b1e32f8c8de12a16fb476d8
Mumuksha Pant <mumukshapan97@gmail.com>
2024-09-24 11:12:08 -0400

    Time: 0 ms (100%), Space: 41.8 MB (22.74%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,26 +1,24 @@
 class Solution {
     public String longestCommonPrefix(String[] s) {
-//     int i=0; 
-     int j=0; 
-     int n= s.length; 
-     int len=0; 
-     Arrays.sort(s); 
-     String s1= s[0];     // s[0]= flight, flow, flower
-     String s2= s[n-1] ; 
-     int i=0; 
 
+        Arrays.sort(s); //flight , flow, flower
 
+        int i=0; 
+        int n= s.length; 
 
+        String s1= s[0];   //f l i g h t
+        String s2= s[n-1];// f l o w e r 
 
-     while(i<s1.length()  && i<s2.length()){
-        if(s1.charAt(i)==s2.charAt(i)){
-            i++; 
+        while(i<s1.length() && i<s2.length() ){
+            if(s1.charAt(i)==s2.charAt(i))
+                 i++; 
+
+            else 
+                break ; 
         }
-        else break ; 
-     }
+        return s1.substring(0,i); 
 
 
-     return s1.substring(0, i); 
-       
+
     }
 }
