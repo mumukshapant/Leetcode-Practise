d2197330995133864ff02fe51af2c77ff697c213
Mumuksha Pant <mumukshapan97@gmail.com>
2024-09-24 15:24:22 -0400

    Time: 27 ms (25.73%), Space: 45.3 MB (17.95%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minEatingSpeed(int[] nums, int h) {
+        int low=0;
+        int high=Integer.MIN_VALUE;
+        for(int e:nums){
+            high=Math.max(high,e);
+        }
+        while(low<=high){
+            int mid= low+(high-low)/2; 
+            int totaltime=0; 
+
+
+            for(int i=0;i<nums.length;i++){
+                totaltime+=Math.ceil((double)(nums[i]) /(double)(mid));
+            }
+
+            if(totaltime<=h)
+                high=mid-1; 
+            
+            else    low=mid+1; 
+
+
+        }
+        return low; 
+    }
+}
