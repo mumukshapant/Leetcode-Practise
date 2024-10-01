0841cbb80561039af11e8ecb781e43a2fa6278c6
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-01 09:01:29 -0400

    Time: 53 ms (5.02%), Space: 41.6 MB (16.92%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,41 +1,43 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        
-        int low=0; 
-        int high=0; 
+        int low = 0;
+        int high = 0;
 
-        for(int i:nums)
-        {
-            low=Math.max(low, i); 
-            high+=i; 
+        for (int i : nums) {
+            low = Math.max(low, i);
+            high += i;
         }
+        // mid represents the largest sum possible
 
-        while(low<=high){
-            int mid=low+(high-low)/2; 
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            for (int i = 0; i < nums.length; i++) {
+                if (countsubarray(nums, mid) > k) 
+                    low=mid+1; 
 
-            for(int i=0;i<nums.length;i++){
-                if(number_of_subarray(nums, mid) > k )  
-                low=mid+1; // if the number of subarray count is greater than the k , means mid  ( ie the sum) is too low & we need to increase the lower bound, ie, low =mid+1;
-
-                else   high=mid-1; 
+                else 
+                    high=mid-1; 
+                
             }
-        }
-        return low; 
-        
+
+        }return low; 
+
     }
-    private int number_of_subarray(int[] nums, int k){
-        int count=1; //min 1 subarray is there
-        int sum=nums[0]; 
 
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]+sum> k){
-                count++; 
-                sum=nums[i];
-            }else{
-                sum+=nums[i]; 
+    private int countsubarray(int[] nums, int maxsum) {
+        int count = 1;
+        int sum = nums[0];
+        // k is the max expected sum
+        for (int i = 1; i < nums.length; i++) {
+
+            if (sum + nums[i] > maxsum) {
+                count++; // means another subarray
+                sum = nums[i];
+            } else {
+                sum += nums[i];
             }
         }
 
-        return count; 
+        return count;
     }
 }
