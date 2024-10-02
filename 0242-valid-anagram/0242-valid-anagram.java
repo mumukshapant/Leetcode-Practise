604f7ab671f54466a5f10908df448e12456dfc7f
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-02 11:03:27 -0400

    Time: 9 ms (34.84%), Space: 42.9 MB (80.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        s=s.toLowerCase(); 
+        if(s.length()!=t.length())
+        return false; 
 
-        if (s.length() != t.length())
-            return false;
-        int[] count = new int[26]; // Assuming the strings contain only lowercase English letters
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+        int[] arr= new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++; 
+            arr[t.charAt(i)-'a']--; 
         }
-        for (int c : count) {
-            if (c != 0) {
-                return false;
-            }
+
+        for(int c: arr){
+            if (c!=0)
+            return false; 
         }
-return true;
+
+return true; 
+
+
+        
     }
 }
