8077fa366a16901c9766ec0838d332f77c2d4514
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-18 13:30:55 -0400

    Time: 0 ms (100%), Space: 42.6 MB (7.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,31 +1,50 @@
 class Solution {
     public int search(int[] nums, int target) {
+        int n= nums.length; 
+        //find point of rotation 
         int low=0; 
-        int high=nums.length-1; 
+        int high= n-1; 
+
         while(low<=high){
             int mid= low+(high-low)/2; 
 
             if(nums[mid]==target)
                 return mid; 
-            
-            //sorted -- right side 
-            if(nums[high]>=nums[mid]){
 
-                if(target>nums[mid] && target<=nums[high])
+
+
+            //left side unsorted BUT right sorted 
+            if(nums[low]>nums[mid]){
+                if(target>nums[mid] && target <= nums[high]) //means target is in left side
                     low=mid+1; 
-                else high= mid-1; 
+                else//T in right
+                    high=mid-1; 
 
-            }
 
-            //sorted left 
-            if(nums[low]<=nums[mid]){
-                if(target<nums[mid] && target>=nums[low])
-                    high= mid-1; 
-                else 
-                    low = mid+1; 
+
+            }else if(nums[mid]>nums[high]) {
+            //right side unsorted, but left sorted 
+            
+
+            if (target >= nums[low] && target < nums[mid])
+                high=mid-1; 
+            else low=mid+1; 
                 
             }
+        
+        // If neither side is rotated, perform normal binary search adjustments
+            else {
+                if (target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        
+      
+        
         }
-        return -1; 
+        return -1;
+        
     }
 }
