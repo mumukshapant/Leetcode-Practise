4d52e19603190d4a7cc81bc4e17bb2d861c1c8ec
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-18 14:53:34 -0400

    Time: 0 ms (100%), Space: 46.1 MB (18.13%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,61 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+       int[] res= new int[]{lowerbound(nums, target), upperbound(nums, target)}; 
+
+       return res; 
        
-       int lb= lowerbound(nums, target); 
-       int ub= upperbound(nums, target); 
-
-        return new int[]{lb,ub}; 
-        
     }
-
-    private int lowerbound(int[] nums, int target){
-         int low=0; 
-         int i=-1; 
+    private int lowerbound(int[] nums , int t){
+        int low=0; 
         int high= nums.length-1; 
-        int[] res= new int[2]; 
-        
+        int i=-1; 
         while(low<=high){
-            int mid=low+(high-low)/2; 
-            if(nums[mid]==target){
-                i=mid;
+            int mid= low+(high-low)/2; 
+            if(nums[mid]==t){
+                i=mid; 
                 high=mid-1; 
-               
-            }else if (nums[mid]<target)
-                low=mid+1; 
+            }else if (nums[mid]>t) {
+                high=mid-1; 
+            }else{low=mid+1; }
+        }
 
 
-            else
-                high=mid-1;
-            
-
-    }
-        return i; }
-
-
-    private int upperbound(int[] nums, int target){
-         int low=0; 
-         int i=-1; 
-        int high= nums.length-1; 
-        int[] res= new int[2]; 
-        
-        while(low<=high){
-            int mid=low+(high-low)/2; 
-            if(nums[mid]==target){
-                i=mid;
-                low=mid+1; 
-                
-               
-            }else if (nums[mid]<target)
-                low=mid+1; 
-
-
-            else
-                high=mid-1;
-            
-
-    }
         return i; 
-}
+    }
 
-
+    private int upperbound(int[] nums, int t){
+         int low=0; 
+        int high= nums.length-1; 
+        int i=-1; 
+        while(low<=high){
+            int mid= low+(high-low)/2; 
+            if(nums[mid]==t){
+                i=mid; 
+                low=mid+1; 
+            }else if (nums[mid]>t) {
+                high=mid-1; 
+            }else{low=mid+1; }
+        }
+        return i; 
+    }
 }
