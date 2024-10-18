43d27ce67882ea118816b40b315c84ce610a5394
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-18 10:42:16 -0400

    Time: 10 ms (91.84%), Space: 47.4 MB (21.42%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,44 +1,37 @@
 class Solution {
     public int shipWithinDays(int[] nums, int days) {
-        int low = 0;
+
+        int low = Integer.MIN_VALUE;
         int high = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            low = Math.max(nums[i], low);
             high += nums[i];
+            low = Math.max(nums[i], low);
         }
 
-        int ans = high;
+        int res = high;
 
         while (low <= high) {
-            int capacity = low + (high - low) / 2;
-            int reqdays = 1;
+            int mid = low + (high - low) / 2; // curr selected capacity
             int sum = 0;
+            int reqdays = 1;
 
             for (int i = 0; i < nums.length; i++) {
-                if (nums[i] + sum <= capacity) 
-                    sum += nums[i]; // since curr sum capacity se kam hai, to we can add more boxes
-                else {
-
+                if (sum + nums[i] <= mid) {
+                    sum += nums[i];
+                } else {
                     reqdays++;
-                    sum = nums[i]; }
-                
-            }// for loop closing
+                    sum = nums[i];
+                }
+            }
 
-            // outside the FOR loop
-
-            if (reqdays <= days) { // means capacity zyada li hai humne, issi liye required days kam hain
-                ans = capacity;
-                high = capacity - 1;
-
-            } else
-                low = capacity + 1;
-
-
-
+            if (reqdays <= days) {
+                high = mid - 1;
+                res = mid;
+            } else {
+                low = mid + 1;
+            }
         }
-
-        return ans;
-
+        return res;
     }
 }
