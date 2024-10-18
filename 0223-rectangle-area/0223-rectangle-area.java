a9a52c47750746d1b7b08b565f4fc3d8f0040cc3
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-18 12:55:05 -0400

    Time: 1 ms (100%), Space: 42.5 MB (55.79%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -1,15 +1,16 @@
 class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
 
-        int area1= (ax2-ax1)*(ay2-ay1);
-        int area2= (bx2-bx1)*(by2-by1); 
+        int a1 = (ax2 - ax1) * (ay2 - ay1);
+        int a2 = (bx2 - bx1) * (by2 - by1);
 
-        int cx1=Math.max(ax1,bx1);
-        int cx2=Math.min(ax2,bx2);
-        int cy1=Math.max(ay1,by1);
-        int cy2=Math.min(ay2,by2);
 
-        int commonlength= cx2-cx1 ;
+    
+                int cx1 = Math.max(ax1, bx1);
+                int cx2 = Math.min(ax2, bx2);
+                int cy1 = Math.max(ay1, by1);
+                int cy2 = Math.min(ay2, by2);
+int commonlength= cx2-cx1 ;
         int commonheight= cy2-cy1 ; 
 
         int overlaparea=0; 
@@ -17,9 +18,8 @@ class Solution {
         if(commonlength>0 &&  commonheight>0)
          overlaparea= commonlength*commonheight; 
 
-        return area1+area2-overlaparea; 
-        
-    }
 
-   
+        return a1 + a2 - overlaparea;
+
+    }
 }
