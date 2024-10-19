39c8bad5cbf1ecc82faa0128e24d7599a104060d
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-19 16:56:08 -0400

    Time: 5 ms (5.33%), Space: 41.7 MB (10.47%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,13 +1,30 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i=0, j=0,count=0; 
-        if(t.length()<s.length()) return false; 
-        while(i<t.length() && j<s.length()){
-            if(t.charAt(i)==s.charAt(j))
-            {   count++; 
-                i++; j++; 
-            }else{i++; }
+        int a = s.length(); 
+        int b = t.length(); 
+
+        boolean[][] dp = new boolean[a + 1][b + 1]; 
+
+        for (int i = 0; i <= b; i++) {
+            dp[0][i] = true;
         }
-        return count==s.length(); 
+
+        for (int i = 1; i <= a; i++) {
+            dp[i][0] = false;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];  // Use matching characters
+                } else {
+                    dp[i][j] = dp[i][j - 1];  // Skip current character in `t`
+                }
+            }
+        }
+
+        // Return the result for the entire strings `s` and `t`
+        return dp[a][b];
     }
 }
