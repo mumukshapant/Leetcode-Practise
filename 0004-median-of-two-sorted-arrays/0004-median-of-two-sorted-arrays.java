5e7e16f736fdd7580797fca7dbea251527e26583
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-21 10:36:59 -0400

    Time: 2 ms (62.39%), Space: 46 MB (56.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,50 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-  
-        int m = nums1.length;
-        int n = nums2.length;
-          // Ensure nums1 is the smaller array
-        if (m > n) {
-            return findMedianSortedArrays(nums2, nums1);
+        int n1=nums1.length; 
+        int n2=nums2.length; 
+        int[] newarr = new int[n1+n2]; 
+        int i=0; int j=0; int k=0;  
+        while(i<n1 && j<n2){
+            if(nums1[i]>nums2[j]){
+                newarr[k]=nums2[j]; 
+                k++; 
+                j++; 
+            }else{
+                newarr[k]=nums1[i]; 
+                k++; i++; 
+            }
+        }
+        //remaining elements
+        while(i!=n1){
+            newarr[k++]=nums1[i++]; 
+        }
+        while(j!=n2){
+            newarr[k++]=nums2[j++]; 
         }
 
-        int length = n + m;
-        int half = (length+1) / 2;
-        int low = 0;
-        int high = m;
-        double median = 0;
-
-        while (low <= high) {
-
-             int mid = low + (high - low) / 2; // of smaller array
-            int midlarger = half - mid;
-
-            // Edges handling
-            int l1 = (mid == 0) ? Integer.MIN_VALUE : nums1[mid - 1];
-            int l2 = (midlarger == 0) ? Integer.MIN_VALUE : nums2[midlarger - 1];
-            int r1 = (mid == m) ? Integer.MAX_VALUE : nums1[mid];
-            int r2 = (midlarger == n) ? Integer.MAX_VALUE : nums2[midlarger];
-
-                  
-        
-            // condition satisfy l1<=r2 && l2<=r2
-            if (l1 <= r2 && l2 <= r1) {
-
-                // even length
-                if (length % 2 == 0)
-                    return  (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                else
-                    return Math.max(l1, l2);
-      
-            } else if (l1 > r2)
-                high = mid - 1;
-
-            else
-                low = mid + 1;
-
+        int n= newarr.length;
+        if (n%2!=0)
+            return (double)newarr[n/2]; 
+        else {
+            int m1=n/2-1;  
+            int m2=n/2;
+            return (double)(newarr[m1]+newarr[m2])/2.0;
         }
-        return median;
+
+
+
+
 
     }
 }
