e6133f54754911eb5ee947f296affc768749513b
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-24 16:21:47 -0400

    Time: 9 ms (94.71%), Space: 56.8 MB (51.99%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,36 +1,77 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        //brute 
-    /**
-    1. copy all list el to arr 
-    2. sort arr
-    3. put them back in list
-     */
-     ListNode curr=head; 
-        List<Integer> arr= new ArrayList<>(); 
+        /**
+         * optimal soln
+         * 1. create a function mergeSortedList()-- 2ptr approach
+         * 2. findmiddle ()-- slow fast ptr
+         * 3. right=middle.next; left=head
+         * 4. sort (left) , sort(right)
+         */
 
-     while(curr!=null){
-        arr.add(curr.val); 
-        curr=curr.next; 
-     }
-     Collections.sort(arr); 
-     curr=head; 
-     int i=0; 
-     while(curr!=null){
-        curr.val=arr.get(i); 
-     i++; 
-     curr=curr.next; 
-     }
-     return head; 
+         if (head == null || head.next == null) 
+              return head;
+        
+        ListNode mid= findmiddle(head); 
+        ListNode right= mid.next; 
+        ListNode left= head; 
+
+        mid.next=null;
+
+        left=sortList(left); 
+        right=sortList(right); 
+        
+        return mergetwolists(left, right ); 
+    
+    
+
+
+    }
+
+    private ListNode mergetwolists(ListNode list1, ListNode list2) {
+        
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+        
+            temp = temp.next;
+        }
+
+      
+        if (list1 != null) 
+            temp.next = list1;
+         else 
+            temp.next = list2;
+        
+
+        return dummyNode.next;
+    }
+
+    private ListNode findmiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+
     }
 }
