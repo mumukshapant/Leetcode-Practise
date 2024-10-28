435751591d435f6621ebb6bbccbecf3fa56634ca
Mumuksha Pant <mumukshapan97@gmail.com>
2024-10-28 15:52:06 -0400

    Time: 12 ms (95.12%), Space: 42.1 MB (90.06%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -1,15 +1,40 @@
 class Solution {
     public int numTeams(int[] nums) {
+        // leftsmaller+leftgreater+1+rightsmaller+rightgreater=n
+
         int n= nums.length; 
-        int count=0; 
-        for(int i=0;i<n;i++){
-            for (int j=i+1; j<n; j++){
-                for(int k=j+1;k<n;k++){
-                    if((nums[i]<nums[j] && nums[j]<nums[k] )|| (nums[i]>nums[j] && nums[j]>nums[k]))            
-                    count++; 
-                }
+        int count=0;
+        int leftgreater=0, rightsmaller=0; 
+        int countincreasingorder=0, countdecreasingorder=0;
+
+        for(int mid = 1; mid< n-1;mid++){
+            //left smaller 
+            int leftsmaller=0; 
+            for(int i=0;i<mid;i++){
+                if(nums[i]<nums[mid])
+                    leftsmaller++;
             }
-        }
+
+            //right greater
+            int rightgreater=0; 
+            for(int i=mid+1;i<n;i++){
+                if(nums[i]>nums[mid])
+                    rightgreater++; 
+            }
+
+            count+= leftsmaller*rightgreater; 
+            
+            leftgreater= mid-leftsmaller; 
+            rightsmaller= n-1-mid-rightgreater; 
+
+            count += leftgreater*rightsmaller; 
+
+
+
+
+        } 
+       
+       
         return count; 
     }
 }
