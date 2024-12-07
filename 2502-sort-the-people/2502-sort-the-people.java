91914853dc983391a73a6375fe2ac4d12ce01247
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-07 18:28:01 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,22 +1,13 @@
 class Solution {
-    public String[] sortPeople(String[] names, int[] heights) {
-        // Map<String, Integer> map = new HashMap<>(); 
-        // for(int i=0;i<names.length;i++){
-        //     map.put(names[i], heights[i]); 
-        // }
-        //Collections.sort(map); 
-        String[] res= new String[names.length]; 
-        for(int i=0;i<names.length;i++){
-            for(int j=0;j<heights.length;j++){
-            if(heights[i]>heights[j]){
-                int temp= heights[j]; 
-                heights[j]=heights[i]; 
-                heights[i]=temp; 
-
-                String stemp = names[j]; 
-                    names[j]= names[i]; 
-                    names[i]= stemp; 
-            }
+    public String[] sortPeople(String[] names, int[] nums) {
+        String[] temp = new String[nums.length]; 
+        for(int i=0;i<nums.length; i++){
+            for(int j=i;j<nums.length; j++){
+                if(nums[i]<nums[j]){
+                    String t= names[i]; 
+                    names[i]=names[j]; 
+                    names[j]=t; 
+                }
             }
         }
         return names; 
