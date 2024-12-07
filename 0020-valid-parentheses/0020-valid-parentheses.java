608aec8da3dfc2656580f870413e8cd0b4fcf582
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-07 18:17:16 -0500

    Time: 3 ms (42.88%), Space: 41.9 MB (28.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st= new Stack<>(); 
-        if(s.length()%2!=0)
-        return false; 
+        for(int i=0;i<s.length();i++){   
+            char c= s.charAt(i); //  )
+            if(st.isEmpty() &&( c==')' || c==']' || c=='}'))
+                return false; 
 
-        for(int i=0;i<s.length();i++){
-            char c= s.charAt(i); 
-            if(c=='(' || c=='{' || c=='[')
+            if(c=='(' || c=='[' || c=='{')
                 st.push(c); 
-
-             if((c==')' || c==']' || c=='}')){
-                if (st.isEmpty()) {
-                    return false;
-                }
-
-
-                int top= st.pop(); 
-                if((c==')' && top!='(') || (c==']' && top!='[') || (c=='}' && top!='{'))
+            if(!st.isEmpty() && (c==']' || c==')' || c=='}')){
+                char top = st.pop(); 
+                if(top=='(' && c!=')')
                     return false; 
+                if(top=='[' && c!=']')
+                    return false; 
+
+                if(top=='{' && c!='}')
+                return false; 
             }
-
-         
-
         }
         return st.isEmpty(); 
-        
     }
 }
