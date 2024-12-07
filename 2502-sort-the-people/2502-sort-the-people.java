99a4c45d5c60efcba3b780b99a5e4c81b5be0d3f
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-07 18:30:22 -0500

    Time: 31 ms (6.01%), Space: 45.6 MB (23.99%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,9 +1,15 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] nums) {
-        String[] temp = new String[nums.length]; 
+       // String[] temp = new String[nums.length]; 
         for(int i=0;i<nums.length; i++){
-            for(int j=i;j<nums.length; j++){
-                if(nums[i]<nums[j]){
+            for(int j=0;j<nums.length; j++){
+                if(nums[i]>nums[j]){
+
+                    int temp= nums[i]; 
+                    nums[i]= nums[j]; 
+                    nums[j]= temp; 
+
+
                     String t= names[i]; 
                     names[i]=names[j]; 
                     names[j]=t; 
