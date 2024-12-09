3036d2896d8c1dcb7793c44dd988fa1d09bb621b
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-09 14:30:06 -0500

    Time: 9 ms (50.97%), Space: 46.8 MB (13.91%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,28 @@
 class Solution {
     public int[][] merge(int[][] nums) {
-        Arrays.sort(nums, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(nums, (a,b)->Integer.compare(a[0],b[0] )); 
+        int[] curr= nums[0]; // curr= [1,3]
+
+        List<int[]> l= new ArrayList<>(); 
         
-        List<int[]> list= new ArrayList<>(); 
-
-        int[] curr = nums[0]; // [1,3]
-
-        for (int i = 0; i < nums.length; i++) {
-            if (curr[1] >= nums[i][0]) {
-                // merge
-                curr[1] = Math.max(nums[i][1], curr[1]); // new interval
-            } else {// no merge, move curr
-            list.add(curr); 
-                curr = nums[i];
+        // /[1,6],[2,3]
+        for(int i=1;i<nums.length;i++){
+            if(curr[1] >= nums[i][0] ){
+                curr[1]= Math.max(curr[1], nums[i][1]); //max interval will b the ending interval
+               // l.add(curr[1]); 
             }
+            else
+                {
+                l.add(curr); 
+                curr=nums[i]; 
+                }
+
         }
-         
-                 list.add(curr);
+        l.add(curr); 
+
 
         
-        return list.toArray(new int[list.size()][]);
-
+     return l.toArray(new int[l.size()][]);
 
     }
 }
