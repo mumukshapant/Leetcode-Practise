3d9629a28704ee9e631255bb8b6c93f89aac16a4
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-09 14:04:08 -0500

    Time: 20 ms (9.48%), Space: 44 MB (16.9%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,9 +1,12 @@
 import java.math.BigInteger; 
+
 class Solution {
     public String addStrings(String num1, String num2) {
         BigInteger b1= new BigInteger(num1); 
-                BigInteger b2= new BigInteger(num2);
-                return String.valueOf(b1.add(b2)); 
+        BigInteger b2= new BigInteger(num2); 
 
+        // int x= Integer.parseInt(num1)+Integer.parseInt(num2);
+        // String res=Integer.toString(x); 
+        return String.valueOf(b1.add(b2));
     }
 }
