60914f9d6eabfcecc1bbabe68413dc7494435af3
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-12 14:19:46 -0500

    Time: 1 ms (75.97%), Space: 46 MB (7.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,47 +1,45 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        boolean[] rowarr = new boolean[row];
-        boolean[] colarr = new boolean[col];
-
-
-        // STEP 1 : identify the row and col that need to be zeroed out
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matrix[i][j] == 0) {
-                    rowarr[i] = true;
-                    colarr[j] = true;
-                }
-            }
-        }
-
-
-        //STEP 2:  Set the identified rows to zero
-        for (int i = 0; i < row; i++) {
-            if (rowarr[i]) { // if row is 0 
-                for (int j = 0; j < col; j++) {
-                    matrix[i][j] = 0;
-                }
-                
-            }
-        }
-
-
-        // STEP 3: Set the identified cols to zero
-        for (int j = 0; j < col; j++) {
-            if (colarr[j]) {
-                for (int i = 0; i < row; i++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-
-
-
-
-
+    public void setZeroes(int[][] mat) {
+        int m = mat.length; 
+        int n = mat[0].length; 
+        
+        for (int i = 0; i < m; i++) 
+            for (int j = 0; j < n; j++) 
+                if (mat[i][j] == 0) 
+                    setzero(mat, i, j); // Mark the entire row and column 
+                    
+        finalizeZeroes(mat); 
     }
+
+    private void setzero(int[][] mat, int row, int col) {
+        int m = mat.length; 
+        int n = mat[0].length; 
+        
+        // Mark  row
+        for (int i = 0; i < n; i++) {
+            if (mat[row][i] != 0) 
+                mat[row][i] = -1989;  // -1989 is visited
+            
+        }
+        
+        //  column
+        for (int i = 0; i < m; i++) {
+            if (mat[i][col] != 0) 
+                mat[i][col] = -1989;  
+        }
+    }
+
+    // Final pass to set all marked rows and columns to zero
+    private void finalizeZeroes(int[][] mat) {
+        int m = mat.length; 
+        int n = mat[0].length; 
+        
+        for (int i = 0; i < m; i++) 
+            for (int j = 0; j < n; j++) 
+                if (mat[i][j] == -1989) 
+                    mat[i][j] = 0;
+
+}
+
+
 }
