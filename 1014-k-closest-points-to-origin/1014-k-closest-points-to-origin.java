4b978a71bc0391444b255b8ac4a5aff0ac672c4e
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-11 21:58:21 -0500

    Time: 30 ms (61.21%), Space: 53.1 MB (48.82%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,30 +1,30 @@
 class Solution {
     public int[][] kClosest(int[][] nums, int k) {
+        
+        int[][] dist= new int[nums.length][3];
 
-        // [ (x2-x1)^2 + ( y2-y21)^2 ] ^(-1/2)
-                int[][] distances = new int[nums.length][3];
+        for(int i=0;i<nums.length ; i++){
+            int x= nums[i][0]; // 1
+            int y= nums[i][1]; // 3
+            
+            int d= x*x + y*y; 
+            
+            dist[i][0]=d; 
+            dist[i][1]=x; 
+            dist[i][2]=y;
+       }
 
+       //d ke basis par "dist" array ko sort karo 
+       Arrays.sort(dist, (a,b) ->Integer.compare( a[0],b[0] )   );
 
-        int min = Integer.MAX_VALUE;
+       int[][] res= new int[k][2]; 
 
-        for (int i = 0; i < nums.length; i++) {
-            int x = nums[i][0];
-            int y = nums[i][1];
+       for(int i=0;i<k; i++){
+        res[i][0]=dist[i][1]; 
+        res[i][1]= dist[i][2]; 
+       }
 
-            int dist = x * x +  y * y;
-
-            distances[i][0] = dist;
-            distances[i][1] = x;
-            distances[i][2] = y;
-        }
-        Arrays.sort(distances, (a, b) -> Integer.compare(a[0], b[0]));
-        // k closest points
-        int[][] result = new int[k][2];
-        for (int i = 0; i < k; i++) {
-            result[i][0] = distances[i][1];
-            result[i][1] = distances[i][2];
-        }
-        return result;
+       return res;
 
     }
 }
