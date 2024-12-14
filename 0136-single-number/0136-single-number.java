9db00e10cb189255ee3681f81ef8732f616c1b1b
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-14 09:59:20 -0500

    Time: 1 ms (99.86%), Space: 46.8 MB (9.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        //using property : a^a = 0 && a^0=a
-        // XOR is : 
+// XOR 
 
-        // 0 0  |  0
-        // 1 0  |  1
-        // 0 1  |  1
-        // 1 1  |  0
+//  0 0 -- 0 
+//  1 1 -- 0 
+//  0 1    1
+//  1 0    1 
+ 
+ int xor=0; 
 
-        int xor=0; 
-        for(int i=0;i<nums.length;i++){
-             xor=xor^nums[i]; 
-        }
-        return xor; 
-    }
+ for(int i=0;i<nums.length;i++){
+   xor = xor^nums[i]; 
+ }
+ return xor; 
+
+}
 }
