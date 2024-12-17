230b7085f0a3b9630aef45831c0f030a9c59c770
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-17 09:37:03 -0500

    Time: 0 ms (100%), Space: 41.3 MB (7.51%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,6 +1,15 @@
 class Solution {
     public int hammingWeight(int n) {
-        return Integer.bitCount(n); 
+        //int data type is a 32-bit signed 
+        // & is bitwise AND
+        // sets the result bit to 1 only if both bits are 1. Otherwise, the result is 0.
         
+        int count=0; 
+        for(int i=0;i<32 ; i++){
+            if(( ( n>> i )& 1 ) ==1 )
+                count++; 
+
+        }
+        return count;
     }
 }
