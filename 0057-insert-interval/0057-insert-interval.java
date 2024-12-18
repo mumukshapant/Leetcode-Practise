ba4592267531f30deedb0c0d59ae48e8b2441c14
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-18 11:00:35 -0500

    Time: 3 ms (21.44%), Space: 45.2 MB (15.4%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,31 +1,29 @@
 class Solution {
     public int[][] insert(int[][] nums, int[] newinterval) {
 
-        int[] curr = newinterval; // Start with the new interval
-        Arrays.sort(nums, (a, b) -> Integer.compare(a[0], b[0])); // Sort intervals
+        int[] curr = newinterval; 
+        Arrays.sort(nums, (a, b) -> Integer.compare(a[0], b[0])); 
 
         List<int[]> ans = new ArrayList<>();
+
         for (int i = 0; i < nums.length; i++) {
 
-            // If current interval is completely before `newInterval`
-            if (nums[i][1] < curr[0]) {
-                ans.add(nums[i]);
+           if(nums[i][1]>= curr[0] && nums[i][0]<=curr[1]){
+                //merge 
+                curr[0] = Math.min(curr[0], nums[i][0]);
+                curr[1] = Math.max(curr[1], nums[i][1]); 
             }
-            // If current interval is completely after `newInterval`
-            else if (nums[i][0] > curr[1]) {
-                ans.add(curr); // Add the merged interval
-                curr = nums[i]; // Move to the next interval
-            }
-            // If intervals overlap, merge them
-            else {
-                curr[0] = Math.min(curr[0], nums[i][0]); // Update start of merged interval
-                curr[1] = Math.max(curr[1], nums[i][1]); // Update end of merged interval
+            else if(nums[i][0]> curr[1]){
+                // means newinterval is in the front , no merging
+                ans.add(curr); 
+                curr=nums[i];
+            }else{
+                ans.add(nums[i]); 
             }
         }
-
-        // Add the last interval
         ans.add(curr);
 
+       
         return ans.toArray(new int[ans.size()][]);
     }
 }
