b167237105841289d27a8aae28adab733feeee7d
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-18 12:57:32 -0500

    Time: 47 ms (74.18%), Space: 73.9 MB (23.46%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,6 +1,9 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] nums) {
         Arrays.sort(nums, (a,b)->Integer.compare(a[1],b[1])); 
+                      
+// we find the number of intervals which are NOT overlapping. (say 4)
+// this means out of total n=6, 2 deletions need to be made to get 4 NON Overlapping intervals
 
         //if same starting and ending - they are not overlapping 
         int n = nums.length; 
