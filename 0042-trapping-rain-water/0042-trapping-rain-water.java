3dc55b6edb07a5870b84bfe7a6ca03ebefb53e7f
Mumuksha Pant <mumukshapan97@gmail.com>
2024-12-26 13:28:05 -0500

    Time: 0 ms (100%), Space: 46.6 MB (29.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,20 +3,35 @@ class Solution {
         int n = nums.length; 
         int res=0; 
 
-        int[] left= new int[n]; 
-        left[0]=nums[0]; 
-        int[] right= new int[n]; 
-        right[n-1]=nums[n-1]; 
+        // int[] left= new int[n]; 
+        // left[0]=nums[0]; 
+        // int[] right= new int[n]; 
+        // right[n-1]=nums[n-1]; 
 
-        for(int i=1;i<n; i++){
-            left[i]=Math.max(nums[i], left[i-1]); 
+        // for(int i=1;i<n; i++){
+        //     left[i]=Math.max(nums[i], left[i-1]); 
+        // }
+        // for(int i=n-2; i>=0;i--){
+        //     right[i]=Math.max(nums[i], right[i+1]); 
+        // }
+        int left= Integer.MIN_VALUE; 
+        int right =  Integer.MIN_VALUE; 
+        int i=0, j=nums.length-1; 
+
+        while(i<j){
+            left=Math.max(nums[i], left); 
+            right= Math.max(nums[j], right); 
+
+            if(left<right){
+                res+=left-nums[i]; 
+                i++;
+            }else{
+                res+=right- nums[j]; 
+                j--;
+            }
         }
-        for(int i=n-2; i>=0;i--){
-            right[i]=Math.max(nums[i], right[i+1]); 
-        }
-        for(int i=0;i<nums.length;i++){
-            res+=Math.min(left[i], right[i])- nums[i]; 
-        }
+        //we are doing ( -nums[i] OR -nums[j]) because we need to calculate kitna water store ho raha hai not the entire area which is being covered. 
+
         return res; 
     }
 }
