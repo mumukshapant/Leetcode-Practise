6121b70a17e7ce9cc331234ed47a3e8852a52e54
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-02 15:15:27 -0500

    Time: 30 ms (62.45%), Space: 53.4 MB (44.24%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,30 +1,39 @@
 class Solution {
     public int[][] kClosest(int[][] nums, int k) {
-        
-        int[][] dist= new int[nums.length][3];
+        int n= nums.length ; 
+        int[][] dist= new int[n][3];
 
-        for(int i=0;i<nums.length ; i++){
-            int x= nums[i][0]; // 1
-            int y= nums[i][1]; // 3
-            
-            int d= x*x + y*y; 
-            
-            dist[i][0]=d; 
-            dist[i][1]=x; 
-            dist[i][2]=y;
-       }
+        //sort as per distance
+        PriorityQueue<Integer> pq= new PriorityQueue<>(); 
 
-       //d ke basis par "dist" array ko sort karo 
-       Arrays.sort(dist, (a,b) ->Integer.compare( a[0],b[0] )   );
+        for(int i=0;i<nums.length;i++){
 
-       int[][] res= new int[k][2]; 
+            int x= nums[i][0] ; 
+            int y= nums[i][1] ; 
 
-       for(int i=0;i<k; i++){
-        res[i][0]=dist[i][1]; 
-        res[i][1]= dist[i][2]; 
-       }
+            int d= x*x+y*y ;
 
-       return res;
+            dist[i][0]= d; 
+            dist[i][1]= x; 
+            dist[i][2] = y; 
+
+          //  pq.offer(dist[0]) ;
+
+
+        }
+
+       // distance ke basis par array "dist" ko sort karo 
+        Arrays.sort(dist, (a,b)->Integer.compare(a[0],b[0])) ;
+
+        int[][] res = new int[k][2]; 
+        for(int i=0;i<k; i++){
+
+            res[i][0] = dist[i][1]; 
+            res[i][1]= dist[i][2]; 
+          //  res[i][2]= dist[i][0];
+
+        }
+return res; 
 
     }
 }
