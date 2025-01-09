1e7f78347281d749d2176967b2ab61a34e2b5341
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-09 14:27:28 -0500

    Time: 2 ms (76.85%), Space: 44.2 MB (82.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean isValidSudoku(char[][] b) {
 
+        for(int i=0;i<9; i=i+3)
+            for(int j=0;j<9 ; j=j+3)
+                if(!isBoxValid(b, i, j))
+                    return false; 
+
         for (int row = 0; row < 9; row++) {            
             Set<Character> rowset= new HashSet<>();
             for (int i = 0; i < 9; i++) {
@@ -26,26 +31,6 @@ class Solution {
             }
         }
 
-        for(int square= 0; square<9;square++){
-            Set<Character> seen= new HashSet<>();
-
-            for (int i = 0; i < 3; i++) {
-                for(int j=0; j<3; j++){
-
-                    int row= (square/3)*3 + i ; 
-                    int col= (square%3)*3 +j; 
-
-                    if(b[row][col]=='.')
-                        continue; 
-                    
-                    if (seen.contains(b[row][col])) 
-                        return false;
-
-                    seen.add(b[row][col]);
-                }
-            }
-        }
-
 
     return true; 
 
@@ -53,5 +38,23 @@ class Solution {
     // We will have a unique hashset for every single row 
     
     // row = row%3  WRONG because if row 2 col 2 then 2%3 gives 2 not 0 . therfore integer division . not mod. 
+    }
+
+    private boolean isBoxValid(char[][]b, int row, int col){
+        Set<Character> set = new HashSet<>();
+
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                if(b[i][j]=='.')
+                    continue; 
+                
+                if ( set.contains(b[i][j]) )
+                    return false;
+                
+                set.add(b[i][j]);
+            }
+        }
+            return true; 
+
     }
 }
