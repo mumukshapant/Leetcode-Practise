3f0b7c91862d1f581c271f496bf23d32573e8a35
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-12 14:16:13 -0500

    Time: 8 ms (33.87%), Space: 45.9 MB (27.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,27 +1,40 @@
 class Solution {
     public boolean canFinish(int n, int[][] pre) {
 
-        // Form a graph
+        // This is BFS Based Topological Sort, so using Queue
+   
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
 
+        // filling the adjacency list -- Each course has a list of prereq courses that depend on it...
+
         for (int i = 0; i < pre.length; i++) {
+            //pre[i][0] : course
+            // pre[i][1] : prerequisite
             adj.get(pre[i][0]).add(pre[i][1]);
         }
 
-        int indegree[] = new int[n];
+
+
+        int indegree[] = new int[n]; //  number of prereq for course i
+
+        // for loop iterates over each course (i) and its dependent courses (j)
         for (int i = 0; i < n; i++) {
-            for (int it : adj.get(i)) {
-                indegree[it]++;
+            for (int j : adj.get(i)) {
+                indegree[j]++; // j depends on course i
             }
         }
 
+        // Topological Sort starts from here..
+
 
         Queue<Integer> q = new LinkedList<Integer>();
+
         for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
+            if (indegree[i] == 0) {    
+                // indegree= 0 means no prereq, can be taken immediately
                 q.add(i);
             }
         }
@@ -29,18 +42,15 @@ class Solution {
 
         int topo[] = new int[n];
         int ind = 0;
-        // o(v + e)
+
         while (!q.isEmpty()) {
-            int node = q.peek();
-
-            q.remove();
+            int node = q.remove();
             topo[ind++] = node;
-            // node is in your topo sort
-            // so please remove it from the indegree
-
-            for (int it : adj.get(node)) {
-                indegree[it]--;
-                if (indegree[it] == 0) q.add(it);
+           
+            for (int j : adj.get(node)) {
+                indegree[j]--;
+                if (indegree[j] == 0) // indegree= 0 means no prereq, can be taken immediately
+                    q.add(j);
             }
         }
     return ind==n;
