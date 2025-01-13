13c7c83922f5e33a3b56ae0bdf78f2b9ea611be5
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-12 19:48:51 -0500

    Time: 70 ms (7.99%), Space: 46.6 MB (91.99%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -3,22 +3,28 @@ class Solution {
 
         String s1 = s;
         String s2 = "";
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            s2 += s1.charAt(i);
-        }
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                 
-                if (s1.charAt(j - 1) == s2.charAt(i - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;     // diagonal +1
-                } else {
-                    dp[i][j]=Math.max(dp[i-1][j-1], Math.max(dp[i-1][j] , dp[i][j-1])); 
-                }
+        int n = s1.length();
+
+        // reverse String s1
+        for (int i = n - 1; i >= 0; i--) {
+            s2 = s2 + s1.charAt(i);
+        }
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp.length; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else 
+                    dp[i][j] =Math.max(dp[i-1][j-1], Math.max(dp[i][j-1], dp[i-1][j])); 
             }
         }
 
-        return dp[s1.length()][s2.length()];
+        //System.out.println(s2);
+
+        return dp[n][n];
+
     }
 }
