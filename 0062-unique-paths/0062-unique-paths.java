1ddd9148ac6d074d1f4d4f06b97feeeb0f4b352a
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-15 14:54:13 -0500

    Time: 0 ms (100%), Space: 40.6 MB (32.83%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
 
-        if(m==1 && n==1)
-        return 1; 
+        int[][] dp = new int[m ][n ];
+        
+        for (int i = 0; i < m; i++) 
+            dp[i][0] = 1; // Only one way to move down
+    
+        for (int j = 0; j < n; j++)
+            dp[0][j] = 1; // Only one way to move right
+    
 
-        int[][] dp = new int[m][n];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) { 
 
-   
+                int up = dp[i - 1][j];
+                int left = dp[i][j - 1];
 
-        for(int i =1; i<m; i++)
-            dp[i][0]=1;
+                dp[i][j] = up + left;
 
-        for(int j=1;j<n; j++)
-            dp[0][j]=1; 
-      
-// why are we doing [i][j-1] & [i-1][j] when Right is j+1 and down is i+1 ? 
-// because to reach the current box i,j : we will see its up & left position values.
-        for(int i=1;i<m; i++){
-            for(int j=1; j<n; j++){
-
-                dp[i][j]=dp[i][j-1]+dp[i-1][j];
             }
         }
-return dp[m-1][n-1]; 
-        
+
+        return dp[m - 1][n - 1];
+
     }
 }
