68c3883de07f493381dcf8115942334748e9eb52
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-16 11:50:24 -0500

    Time: 4 ms (63.37%), Space: 48.1 MB (9.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,35 +1,25 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        int m= grid.length; int n= grid[0].length; 
 
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
+        int[][] dp = new int[m][n]; 
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
+        dp[0][0] = grid[0][0]; 
+        
+        for(int i=1;i<m;i++)
+            dp[i][0] = dp[i-1][0] +grid[i][0]; 
 
-                // boundary condition i
-                // if "i" ( row ) is last , it cannot go up only right
-                if (i == m - 1 && j != n - 1)
-                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+         for(int i=1;i<n;i++)
+            dp[0][i] = dp[0][i-1] + grid[0][i]; 
 
-                // boundary condition j
-                else if (j == n - 1 && i != m - 1)
-                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+        for(int i=1; i<m; i++){
+            for(int j=1;j<n; j++){
+                int up = dp[i-1][j]; 
+                int left= dp[i][j-1];
 
-                else if (j != n - 1 && i != m - 1) {
-                    int right = dp[i][j + 1];
-                    int down = dp[i + 1][j];
-                    dp[i][j] = grid[i][j] + Math.min(down, right);
-                }
-
-                else 
-                    dp[i][j]= grid[i][j]; 
-
+                dp[i][j] += Math.min(up,left)  + grid[i][j];; 
             }
         }
-
-        return dp[0][0]; 
-
+        return dp[m-1][n-1]; 
     }
 }
