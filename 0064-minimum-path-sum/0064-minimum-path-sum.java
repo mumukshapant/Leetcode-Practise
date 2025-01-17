52cae1490e4663f6c466a39e80dfe9be06f59b93
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-17 16:24:34 -0500

    Time: 4 ms (63.13%), Space: 47.5 MB (78.51%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -5,6 +5,8 @@ class Solution {
         int[][] dp = new int[m][n]; 
 
         dp[0][0] = grid[0][0]; 
+
+       // dp=1 
         
         for(int i=1;i<m;i++)
             dp[i][0] = dp[i-1][0] +grid[i][0]; 
@@ -17,7 +19,7 @@ class Solution {
                 int up = dp[i-1][j]; 
                 int left= dp[i][j-1];
 
-                dp[i][j] += Math.min(up,left)  + grid[i][j];; 
+                dp[i][j] += Math.min(up,left)  + grid[i][j];
             }
         }
         return dp[m-1][n-1]; 
