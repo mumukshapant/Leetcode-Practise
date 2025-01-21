bc0fa2c506f7ebd9fb770972d8f5a0e609f35b55
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-20 19:29:50 -0500

    Time: 11 ms (91.35%), Space: 45.6 MB (91.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,49 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
+        int n = nums.length;
+        List<Integer>[] bucket = new List[n + 1];
+
         Map<Integer, Integer> map = new HashMap<>();
         for (int i : nums)
             map.put(i, map.getOrDefault(i, 0) + 1);
-        List<Integer>[] bucket = new List[nums.length + 1];
 
-     
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int num = entry.getKey();
-            int freq = entry.getValue();
+        for (int i : map.keySet()) {
+            int freq = map.get(i); // mep.get(key) returns freq
 
-            if (bucket[freq] == null) {
+            // now each bucket represents freq of the element
+            if (bucket[freq] == null)
                 bucket[freq] = new ArrayList<>();
-            }
-            bucket[freq].add(num);
-        }
-        int[] res = new int[k];
-        int count = 0;
 
-        for (int i = bucket.length - 1; i >= 0 && count < k; i--) {
-            if (bucket[i] != null) {
-                for (Integer integer : bucket[i]) {
-                    res[count] = integer;
-                    count++;
-                }
-            }
+            bucket[freq].add(i); // bucket ki frequency mein key ko add karo
         }
+
+        // every bucket represents frequency.
+        // every bucket has element with that frequency.
+        // for highest freq elemtns, we iterate from the last
+
+        int count = 0;
+        int[] res = new int[k];
+
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (Integer num : bucket[i])
+                {if (count < k) {
+                    res[count] = num;
+                    count++;
+                }}
+            }
+
+            // if (count == k) {
+            //     break;
+            // }
+
+        }
+
+   
+
+
 
         return res;
-
     }
-
 }
