d09158902f3945887609716ff6a20a7cc7be43cb
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-21 18:07:11 -0500

    Time: 1 ms (73.93%), Space: 45.8 MB (45.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,45 +1,46 @@
 class Solution {
     public void setZeroes(int[][] mat) {
-        int m = mat.length; 
-        int n = mat[0].length; 
-        
-        for (int i = 0; i < m; i++) 
-            for (int j = 0; j < n; j++) 
-                if (mat[i][j] == 0) 
-                    setzero(mat, i, j); // Mark the entire row and column 
-                    
-        finalizeZeroes(mat); 
+        int m= mat.length; 
+        int n=mat[0].length; 
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0)
+                    setzero(mat, i, j ); 
+            }
+        }
+
+                    finalisemat(mat); 
+
     }
 
-    private void setzero(int[][] mat, int row, int col) {
-        int m = mat.length; 
-        int n = mat[0].length; 
-        
-        // Mark  row
-        for (int i = 0; i < n; i++) {
-            if (mat[row][i] != 0) 
-                mat[row][i] = -1989;  // -1989 is visited
-            
+    private void setzero(int[][] mat, int row, int col){
+
+        int m= mat.length; 
+        int n=mat[0].length; 
+
+        for(int i=0;i<n ; i++){
+            if(mat[row][i]!=0)
+                mat[row][i]=-1987;  
         }
-        
-        //  column
-        for (int i = 0; i < m; i++) {
-            if (mat[i][col] != 0) 
-                mat[i][col] = -1989;  
+
+        for(int i=0;i<m ; i++){
+            if(mat[i][col]!=0)
+                mat[i][col]=-1987;  
+
         }
+
+
     }
 
-    // Final pass to set all marked rows and columns to zero
-    private void finalizeZeroes(int[][] mat) {
-        int m = mat.length; 
-        int n = mat[0].length; 
-        
+    private void finalisemat(int[][] mat)
+    {
+          int m= mat.length; 
+        int n=mat[0].length; 
+
         for (int i = 0; i < m; i++) 
             for (int j = 0; j < n; j++) 
-                if (mat[i][j] == -1989) 
+                if (mat[i][j] == -1987) 
                     mat[i][j] = 0;
-
-}
-
-
+    }
 }
