9e2e9adb9fa1f72075fac9288546d3f48f278afc
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-21 15:53:34 -0500

    Time: 7 ms (81.85%), Space: 44.9 MB (23.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,25 +1,41 @@
 class Solution {
-    int count = 0;
-
+    int offset; 
     public int findTargetSumWays(int[] nums, int t) {
         int n = nums.length;
 
-        helper(nums, t, 0, 0);
-        return count;
-    }
-
-    private void helper(int[] nums, int t, int currsum, int i) {
+        int sum = 0;
         
-
-        if (i == nums.length) {
-            if (t == currsum)
-                count += 1;
-        } 
-        
-        else {
-            helper(nums, t, currsum + nums[i], i + 1);
-            helper(nums, t, currsum - nums[i], i + 1);
+        for (int num : nums) {
+            sum += num;
         }
 
+
+
+        //  to handle negative indices
+         offset = sum;
+
+        // to handle [-sum, sum]
+        Integer[][] memo = new Integer[n][2 * sum + 1];
+
+        return helper(nums, t, 0, 0, memo);
+    }
+
+    private int helper(int[] nums, int t, int currsum, int i, Integer[][] memo) {
+        if (i == nums.length) {
+            return currsum == t ? 1 : 0;
+        }
+
+        int memoIndex = currsum + offset;
+
+        if (memo[i][memoIndex] != null) {
+            return memo[i][memoIndex];
+        }
+
+        int add = helper(nums, t, currsum + nums[i], i + 1, memo);
+        int subtract = helper(nums, t, currsum - nums[i], i + 1, memo);
+
+        memo[i][memoIndex] = add + subtract;
+
+        return memo[i][memoIndex];
     }
 }
