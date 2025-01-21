024773e0e552e7346b8c76ee9dda6fc350701ad3
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-21 18:36:40 -0500

    Time: 3 ms (86.61%), Space: 49.9 MB (34.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,48 @@
 class Solution {
     int count;
-    int n, m;
+    public int numIslands(char[][] g) {
 
-    public int numIslands(char[][] grid) {
-        n = grid.length; 
-        if (n == 0) return 0;
-        m = grid[0].length;
+        int m= g.length; 
+        int n= g[0].length; 
         count = 0; 
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1') {
-                    callbfs(grid, i, j); 
+        for(int i=0;i<m; i++){
+            for(int j=0;j<n;j++)
+                if(g[i][j]=='1')
+                {
+                    helper(g, i, j); 
                     count++; 
                 }
-            }
+                    
         }
 
         return count; 
+        
     }
 
-    public void callbfs(char[][] grid, int i, int j) {
-        // Boundary checks
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1')
-            return;
+    // this helper function is used for exploring the island -- not counting it.. 
+    
+    private int helper(char[][]g, int r, int c )
+    {
 
-        // Mark the cell as visited by setting it to '0'
-        grid[i][j] = '0';
+        int m= g.length; 
+        int n= g[0].length; 
 
-        // Recursively visit all adjacent cells
-        callbfs(grid, i, j + 1);
-        callbfs(grid, i, j - 1); 
-        callbfs(grid, i + 1, j); 
-        callbfs(grid, i - 1, j); 
+        // conditions...dfs will run only on land
+        if(r<0 || c<0 || r>=m || c>=n || g[r][c]!='1' ) 
+            return 0; 
+        
+        // mark visited 
+        g[r][c] = '2'; 
+
+        for(int[] d: directions){
+            int newrow= d[0]+r; 
+            int newcol= d[1]+c; 
+
+            helper(g, newrow, newcol);
+        }
+        return count ;
+        
     }
+    int[][] directions= new int[][]{{-1,0}, {1,0},{0,1}, {0,-1}};
 }
