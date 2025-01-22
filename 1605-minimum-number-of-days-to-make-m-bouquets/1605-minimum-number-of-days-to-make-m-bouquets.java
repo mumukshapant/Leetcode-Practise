671c865a7853ccfae96687733ec6864ec0c5fbcc
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-22 18:02:35 -0500

    Time: 19 ms (65.84%), Space: 58.7 MB (32.09%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,48 @@
 class Solution {
     public int minDays(int[] nums, int m, int k) {
 
-        // If there are not enough flowers to make the required number of bouquets
-        if (m * k > nums.length) {
+        
+
+        int n = nums.length, low=Integer.MAX_VALUE, high=Integer.MIN_VALUE,ans=-1;
+
+        if (m * k > n) {
             return -1;
         }
 
-        // Set the binary search bounds
-        int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
-        
-        // Find the minimum and maximum val from array 
-        for (int num : nums) {
-            low = Math.min(low, num);
-            high = Math.max(high, num);
+        for(int i:nums){
+            low= Math.min(i, low); 
+            high= Math.max(i, high); 
         }
 
-        int ans = -1; 
+        while(low<=high){
+            int count=0, bouquet=0; 
+            int bloomday= low+(high-low)/2; 
 
-        // Binary search
-        while (low <= high) {
-            int mid = low + (high - low) / 2; 
-            int count = 0; 
-            int bouquet = 0; 
+            for(int i=0;i<n; i++){
 
-            // Try to create bouquets with bloom day <= mid
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] <= mid) {
-                    count++;  // Count consecutive flowers that bloom by `mid`
+                if(nums[i]<=bloomday){
+                    //means the flower has bloomed 
+                    count++; 
 
-                    if (count == k) {  // If we have enough flowers for one bouquet
-                        bouquet++;
-                        count = 0;  // Reset flower count for the next bouquet
-                    }
-                } else {
-                    count = 0;  // Reset count if the flower hasn't bloomed
+                    if(count==k)
+                        {
+                            bouquet++; 
+                            count=0; 
+                        }
+                }else  
+                    count=0 ;  //reset the count
+
                 }
-            }
 
-            // If we can make the required number of bouquets
-            if (bouquet >= m) {
-                ans = mid;  // This is a possible answer
-                high = mid - 1;  // Try to find an earlier possible day
-            } else {
-                low = mid + 1;  // Try a later day
-            }
+            if(bouquet >= m ){
+                ans = bloomday; 
+                high = bloomday-1; 
+            }else
+                low= bloomday+1; 
+
         }
+        return ans;
 
-        return ans; 
+        
     }
 }
