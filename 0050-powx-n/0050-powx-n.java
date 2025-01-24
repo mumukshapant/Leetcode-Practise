40c528228016c64bfc915ea2b73d6a38c0569823
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-24 11:32:32 -0500

    Time: 0 ms (100%), Space: 43.4 MB (13.99%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,29 @@
 class Solution {
     public double myPow(double x, int n) {
-        // x^n 
-        return helper(x, (long)n);
+        return power(x, n); 
+        
     }
-    private double helper(double x, long n){
+    private double power(double x, long n){
+
+        double res=0.0; 
+
         if(n==0)
             return 1; 
-        
-        if(n<0) //negative
-            return  1.0/ helper(x, -n) ; 
 
-        if(n%2==0){
-            return helper(x*x, n/2 );
-        }else{
-            return x * helper(x*x, (n-1)/2); 
 
+         //negative   
+        if(n<0){
+            res= 1.0/power(x, -n); 
         }
+
+        if(n>0 && n%2==0 ){
+            res= power(x*x, n/2); 
+        }
+        if(n>0 && n%2!=0){
+            res= power(x*x, (n-1)/2)*x; 
+        }
+
+        return res; 
+
     }
 }
