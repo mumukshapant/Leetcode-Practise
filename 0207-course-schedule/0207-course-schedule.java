516286a2f0d383c7c41f126dc3bd81154c6ede1e
Mumuksha Pant <mumukshapan97@gmail.com>
2025-01-28 13:51:47 -0500

    Time: 69 ms (5.3%), Space: 46 MB (18.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,59 +1,64 @@
 class Solution {
-    public boolean canFinish(int n, int[][] pre) {
+    public boolean canFinish(int n, int[][] prereq) {
 
-        // This is BFS Based Topological Sort, so using Queue
-   
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+        List<List<Integer>> adj= new ArrayList<>(); 
+
+        for(int i=0;i<n ; i++)
+        {
+            adj.add(new ArrayList<>()); 
+        }
+        for(int[] p : prereq){
+            int course= p[0]; // course
+            int prerequisite= p[1]; 
+
+            adj.get(course).add(prerequisite); 
         }
 
-        // filling the adjacency list -- Each course has a list of prereq courses that depend on it...
+        System.out.println("adjacency list"); 
+        for(int i=0;i<n; i++)
+            System.out.println(i+": "+adj.get(i)+" "); 
 
-        for (int i = 0; i < pre.length; i++) {
-            //pre[i][0] : course
-            // pre[i][1] : prerequisite
-            adj.get(pre[i][0]).add(pre[i][1]);
+        int[] indegree= new int[n]; 
+
+
+        //given : courses - labeled from 0 to numCourses - 1.
+
+        for(int i=0;i<n ; i++){
+            for(int j: adj.get(i)){
+                indegree[j]++; 
+
+            }
+        }
+          System.out.println("\n"); 
+
+          System.out.println("Initial Indegree array"); 
+        for(int i=0;i<n; i++)
+            System.out.print(indegree[i]+" "); 
+
+
+        //topological sort starts..
+        Queue<Integer> q= new LinkedList<>(); 
+        for(int i=0;i<n; i++){
+            if(indegree[i]==0)
+                q.add(i); 
         }
 
+        int[] topo = new int[n]; 
+        int ind=0; 
 
+        while(!q.isEmpty()){
+            int node= q.poll(); // [2] 
+            topo[ind++]= node; 
 
-        int indegree[] = new int[n]; //  number of prereq for course i
+            for(int j: adj.get(node)){
+                indegree[j]--; 
 
-        // for loop iterates over each course (i) and its dependent courses (j)
-        for (int i = 0; i < n; i++) {
-            for (int j : adj.get(i)) {
-                indegree[j]++; // j depends on course i
+                if(indegree[j]==0)
+                    q.add(j); 
             }
         }
 
-        // Topological Sort starts from here..
-
-
-        Queue<Integer> q = new LinkedList<Integer>();
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {    
-                // indegree= 0 means no prereq, can be taken immediately
-                q.add(i);
-            }
-        }
-
-
-        int topo[] = new int[n];
-        int ind = 0;
-
-        while (!q.isEmpty()) {
-            int node = q.remove();
-            topo[ind++] = node;
-           
-            for (int j : adj.get(node)) {
-                indegree[j]--;
-                if (indegree[j] == 0) // indegree= 0 means no prereq, can be taken immediately
-                    q.add(j);
-            }
-        }
-    return ind==n;
+        return ind==n; 
         
     }
 }
