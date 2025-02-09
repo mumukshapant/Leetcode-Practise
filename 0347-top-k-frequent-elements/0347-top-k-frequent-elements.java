ac4f2e3d8a6d83f6189a517933c794d4ec3e0ad0
Mumuksha Pant <mumukshapan97@gmail.com>
2025-02-09 11:46:18 -0500

    Time: 12 ms (82.05%), Space: 48.9 MB (33.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,49 +1,43 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int n= nums.length; 
+System.out.print(n); 
+        List<Integer>[] bucket = new List[n+1]; 
+        Map<Integer, Integer> map = new HashMap<>(); 
 
-        int n = nums.length;
-        List<Integer>[] bucket = new List[n + 1];
-
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i : nums)
-            map.put(i, map.getOrDefault(i, 0) + 1);
-
-        for (int i : map.keySet()) {
-            int freq = map.get(i); // mep.get(key) returns freq
-
-            // now each bucket represents freq of the element
-            if (bucket[freq] == null)
-                bucket[freq] = new ArrayList<>();
-
-            bucket[freq].add(i); // bucket ki frequency mein key ko add karo
+        for(int i=0;i<n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1); 
         }
 
-        // every bucket represents frequency.
-        // every bucket has element with that frequency.
-        // for highest freq elemtns, we iterate from the last
+        for(int i: map.keySet()){
+            int freq= map.get(i); // [1] -> 3
 
-        int count = 0;
-        int[] res = new int[k];
+            if(bucket[freq]==null)
+                bucket[freq]= new ArrayList<>(); 
+            
+            bucket[freq].add(i); 
+        }
 
-        for (int i = bucket.length - 1; i >= 0; i--) {
-            if (bucket[i] != null) {
-                for (Integer num : bucket[i])
-                {if (count < k) {
-                    res[count] = num;
-                    count++;
-                }}
+        int[] res= new int[k]; 
+        int count=0; 
+        
+        for(int i=bucket.length-1; i>=0 ; i--){
+            if( bucket[i]!=null) 
+            {
+                for(Integer num: bucket[i]){
+
+                    if(count<k) 
+                    {
+                        res[count]= num; 
+                        count++; 
+                    }
+
+                }
             }
 
-            // if (count == k) {
-            //     break;
-            // }
-
         }
 
-   
-
-
-
-        return res;
+    return res; 
+        
     }
 }
