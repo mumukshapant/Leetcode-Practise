2c832e62249373c501b88194b2b6969e47ffd245
Mumuksha Pant <mumukshapan97@gmail.com>
2025-02-11 15:11:00 -0500

    Time: 38 ms (18.72%), Space: 45.5 MB (12.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,37 +1,30 @@
 class Solution {
     public int characterReplacement(String s, int k) {
 
-       // jiski max freq hai, usko touch nahi karna hai 
+        int n= s.length(); 
+        int i=0, j=0;
+        int maxlen=0, maxfreq=0; 
+        Map<Character, Integer> map = new HashMap<>();  
+        while(j<n)
+        {
+            char c=s.charAt(j); 
+            map.put(c, map.getOrDefault(c, 0)+1); 
 
-       int maxlen=0, maxfreq=0;
-       int n= s.length(); 
-       int i=0, j=0; 
-       Map<Character, Integer> map = new HashMap<>();
+            maxfreq =Math.max(maxfreq, map.get(c)); 
 
-       while(j<n) {
-        char c= s.charAt(j);
+            int window= j-i+1; 
+            
+            if(window-maxfreq>k){
 
-        map.put(c, map.getOrDefault(c,0)+1); 
+                char ch= s.charAt(i); 
+                map.put(ch, map.getOrDefault(ch, 0)-1); 
+                i++; 
+            }
+            maxlen= Math.max(j-i+1, maxlen); 
+            j++; 
 
-        maxfreq= Math.max(maxfreq, map.get(c)) ; 
 
-        int window= j-i+1; 
-
-        //chars needed to be changed is window- maxfreq 
-
-        if(window- maxfreq > k){
-
-            char character= s.charAt(i); 
-            map.put(character,map.get(character)-1); 
-            i++; 
         }
-        maxlen= Math.max(j-i+1, maxlen); 
-        j ++ ;
-       }
-
-       return maxlen; 
-
-        
-
+        return maxlen; 
     }
 }
