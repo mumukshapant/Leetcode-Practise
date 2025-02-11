8b4a2867aa3d454235589c243c1d0043437eecae
Mumuksha Pant <mumukshapan97@gmail.com>
2025-02-11 17:59:46 -0500

    Time: 2 ms (71.17%), Space: 42.5 MB (48.83%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -7,59 +7,70 @@ class Solution {
         Queue<int[]> q = new LinkedList<>();
         int freshorange=0; 
 
+        // 0 representing an empty cell,
+        // 1 representing a fresh orange, or
+        // 2 representing a rotten orange.
+
         for(int i=0;i<m; i++){
             for(int j=0;j<n;j++){
-                if(g[i][j]==2) //if rotten 
-                    q.offer(new int[] {i, j , 0 }); // r, c, time
-                else if ( g[i][j]==1)  // fresh orange 
-                    freshorange++; 
+
+                if(g[i][j]==2) //rotten
+                    q.offer(new int[]{i, j, 0}); 
+                
+                
+                else if(g[i][j]==1) //fresh
+                    freshorange++ ;
             }
         }
 
-        if (freshorange == 0)
-            return 0;
-        
-        return bfs(g, q, freshorange); 
+        if(freshorange==0)
+            return 0; 
 
+        return bfs(g, freshorange , q); 
 
     }
 
-    private int bfs(int[][] g, Queue<int[]> q, int freshorange) {
-        int m = g.length; 
-        int n =g[0].length; 
+    private int[][] directions = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-        int curr_time=0; 
 
+    private int bfs(int[][] g, int freshorange, Queue<int[]> q) {
+
+        int m = g.length;
+        int n = g[0].length;
+        int currtime=0; 
+    
 
         while(!q.isEmpty()){
-
             int[] curr= q.poll(); 
+            
+            int currow=curr[0]; 
+            int currcol=curr[1]; 
+            currtime= curr[2];
 
-            int curr_row= curr[0]; 
-            int curr_col = curr[1]; 
-            curr_time = curr[2]; 
+           //curr ke 4 directions mein jaao , agar gresh hai, toh usse rotten kardo
+           for(int[] d:directions){
+            int newrow=currow+d[0]; 
+            int newcol= currcol+d[1];
 
-            for(int[] d: directions){
-
-                int newrow = d[0] + curr_row;
-                int newcol = d[1] + curr_col; 
-
-                if(newrow<0 || newrow>=m || newcol<0 || newcol>=n 
+            if(newrow<0 || newrow>=m || newcol<0 || newcol>=n 
                 || g[newrow][newcol] != 1 )
                     continue; 
-                
-                q.offer(new int[] {newrow, newcol, curr_time+1});
 
-                g[newrow][newcol]=2; //make it rotten ( visited)
-                freshorange--;  
+            // time++; 
+            q.offer(new int[]{newrow, newcol, currtime+1});
 
-            }
+            //mark visited --rotten
+            g[newrow][newcol] = 2;
+
+            freshorange-- ; 
+           }
 
         }
+        
+        return freshorange == 0 ? currtime : -1;
 
-        return freshorange == 0 ? curr_time : -1;
+
+
     }
-
-        private int[][] directions = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
 }
