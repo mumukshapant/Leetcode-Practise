65aa7e750a6da0d4db016cc21062a3146e0965d5
Mumuksha Pant <mumukshapan97@gmail.com>
2025-02-18 09:13:43 -0500

    Time: 8 ms (33.87%), Space: 46.5 MB (5.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,69 +1,47 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
+    public boolean canFinish(int n, int[][] prereq) {
+        List<List<Integer>> adj = new ArrayList<>();
+         for(int i=0;i<n; i++)
+            adj.add(new ArrayList<>() );
 
-        // course , pre 
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
-        for(int i=0;i<n; i++)
-            adj.add(new ArrayList<>() ); 
-
-        int[] indegree = new int[n]; 
-
-        for(int[] p: prerequisites){
-            int course= p[0]; 
-            int pre = p[1];
-            adj.get(course).add(pre); 
+        for (int[] p : prereq) {
+            int course = p[1];
+            int pre = p[0];
+            adj.get(course).add(pre);
         }
-        //indegree will contains only prerequisite
+        int[] indegree = new int[n]; // contains the preprequisites required to take the course
 
-
-        // indegree [0] = 1
-        // indegree[1] = 0 
-        
-        for(int i=0;i<n;i++){
-            for(int j: adj.get(i))
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j : adj.get(i)) {
                 indegree[j]++; 
-            
-        }
-        
-        Queue<Integer> q = new LinkedList<>(); //stores pre 
-        
-        for(int i=0;i<n ; i++){
-            if(indegree[i]==0) //no prereq
-            {
-                q.add(i); 
             }
         }
-        int[] topo = new int[n]; 
+
+        for(int i=0;i<n;i++){
+            if (indegree[i] == 0)       // you can take the course
+                    q.add(i);
+        }
+
+        int[] topo = new int[n];
         int index=0; 
 
-
         while(!q.isEmpty()){
-            
-            int node = q.poll();   //      [ 0 ]
-            topo[index ++] = node; 
+            int node=q.poll(); 
+            topo[index++]= node; 
 
             for(int j: adj.get(node)){
                 indegree[j]--; 
-                
-                if(indegree[j]==0)
-                    q.add(j); 
+                 if (indegree[j] == 0)       // you can take the course
+                    q.add(j);
+
+
             }
+            
 
         }
-        
-
-        /**
-                course. Pre
-        // adj : [ 1 -> 0  ]
-                 [ 0 -> 1  ]
+        return index==n; 
 
 
-
-
-         */
-         return index==n; 
-        
     }
 }
