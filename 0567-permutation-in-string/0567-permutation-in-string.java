3655a813dbf0f8219c43ef813a9e878d4b6aeb77
Mumuksha Pant <mumukshapan97@gmail.com>
2025-02-23 17:40:13 -0500

    Time: 6 ms (86.77%), Space: 42.7 MB (68.9%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int m= s1.length(); 
+        int n= s2.length(); 
 
-        if(s1.length() > s2.length())
+        if(m >n) 
             return false; 
 
-        int[] map1= new int[26]; 
-        int[] map2= new int[26]; 
 
-        for(int i=0;i<s1.length () ; i++) 
-        {
-            map1[s1.charAt(i)-'a']++; 
-            map2[s2.charAt(i)-'a']++; 
+        int[] freq1= new int[26] ;
+        int[] freq2 = new int[26]; 
+
+        for(int i=0;i<m ;i++){
+            freq1[s1.charAt(i)-'a'] ++; 
+            freq2[s2.charAt(i) - 'a']++;
         }
-
-        if(Arrays.equals(map1, map2))
+        if(Arrays.equals(freq1,freq2))
             return true; 
 
-        for(int i= s1.length() ;i < s2.length(); i++){
-            map2[ s2.charAt(i-s1.length()) -'a'  ]--; 
-            map2[s2.charAt(i)-'a'] ++; 
+        for(int i=m ; i<n; i++){
+            freq2[s2.charAt( i-m ) -'a'] -- ; 
+            freq2[s2.charAt(i)-'a'] ++; 
 
-            if(Arrays.equals(map1, map2))
-            return true; 
+            if(Arrays.equals(freq1,freq2))
+                return true; 
+
         }
-
         return false; 
-        
         
     }
 }
