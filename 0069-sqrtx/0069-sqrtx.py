1b66da4ebd66b7d30c297c4e455b9a8463e26ada
Mumuksha Pant <mumukshapan97@gmail.com>
2025-02-27 15:54:16 -0500

    Time: 4 ms (44.41%), Space: 12.4 MB (80.25%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        #binary search 
+        if x < 2:
+            return x
+        low=0
+        high= x
+        
+        while low<=high: 
+            mid= low+(high-low)//2
+            
+            if mid*mid==x: 
+                return mid
+            elif mid*mid>x: 
+                high=mid-1
+            else:
+                low=mid+1
+        return high
+
+        
