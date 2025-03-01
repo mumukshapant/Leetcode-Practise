76d1fe47b0f5513816e745cd71b727f163723152
Mumuksha Pant <mumukshapan97@gmail.com>
2025-02-28 21:25:12 -0500

    Time: 4 ms (51.48%), Space: 12.4 MB (96.22%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,15 +1,10 @@
-class Solution:
-    def addStrings(self, num1: str, num2: str) -> str:
-        i,j = len(num1)-1,len(num2)-1
-        carry = 0
-        res = []
-        while i >= 0 or j >= 0 or carry :
-            n1 = int(num1[i]) if i >= 0 else 0
-            n2 = int(num2[j]) if j >= 0 else 0
-            num_sum = n1+n2+carry
-            carry = num_sum//10
-            res.append(str(num_sum % 10))
-            i-=1
-            j-=1
-        return ''.join(res[::-1])
-
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1=int(num1)
+        num2= int(num2)
+        return str(num1+num2)
