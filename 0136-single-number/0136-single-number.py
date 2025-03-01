47822a53c758c79f5496db386be2c37141d28567
Mumuksha Pant <mumukshapan97@gmail.com>
2025-03-01 11:17:32 -0500

    Time: 3 ms (68.77%), Space: 13.9 MB (91.03%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,10 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        freq= Counter(nums)
+        # time & space : O(N), O(N)
+        # freq= Counter(nums)
 
-        for i in freq: #i represents the key
-            print(i)
-            if freq[i]==1 :  #value 
-                return i
+        # for i in freq: #i represents the key
+        #     print(i)
+        #     if freq[i]==1 :  #value 
+        #         return i
+
+        # XOR 
+        # space : O(1), time O(N)
+        
+        xor=0
+        for i in nums: 
+            xor=xor^i
+        return xor
         
