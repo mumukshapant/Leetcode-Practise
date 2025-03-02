f56f2c3d695276899074524690e76346ab56b0c4
Mumuksha Pant <mumukshapan97@gmail.com>
2025-03-02 17:37:25 -0500

    Time: 7 ms (73.56%), Space: 12.5 MB (53.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,17 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        #s = "MCM  XC IV"              Output: 1994
-        dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        prev=0
+        dict={
+            'I': 1, 
+            'V': 5, 
+            'X' : 10, 
+            'L': 50, 
+            'C': 100, 
+            'D' : 500, 
+            'M' :1000
+        }
+        curr=0
         res=0
 
-        for i in  s[::-1] : 
-            if dict[i] >= prev: 
-                res+=dict[i]
+        for i in range (len(s)-1, -1, -1): 
+            if dict[s[i]]>=curr: 
+                res+=dict[s[i]]
             else:
-                res-=dict[i]
-            prev=dict[i]
+                res-=dict[s[i]]
+            curr=dict[s[i]]
+        
         return res
-
-
